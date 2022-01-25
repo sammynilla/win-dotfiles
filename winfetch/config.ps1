5cbc4e665d283cb0474aa98961f95fb3f2bea008
Samantha Bartlett <sammynilla@protonmail.com>
2022-01-25 07:29:25 -0500

    winfetch configuration

---
+++ winfetch/config.ps1
@@ -0,0 +1,21 @@
+$logo = "Windows XP"
+$ShowPkgs = @("scoop", "choco")
+$cpustyle = 'bartext'
+$memorystyle = 'bartext'
+@(
+    "os"
+    "computer"
+    "kernel"
+    "uptime"
+    "pkgs"
+    "pwsh"
+    "terminal"
+    "theme"
+    "cpu"
+    "cpu_usage"
+    "gpu"
+    "resolution"
+    "memory"
+    "blank"
+    "colorbar"
+)
