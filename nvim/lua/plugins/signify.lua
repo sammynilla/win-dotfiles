dd4d10c68122687bdc658d8b98da5216ac027481
Samantha Bartlett <sammynilla@protonmail.com>
2022-05-03 06:57:04 -0400

    edits to signify"

--- nvim/lua/plugins/signify.lua
+++ nvim/lua/plugins/signify.lua
@@ -1,25 +1,19 @@
------------------------------------------------------------
--- Gitsigns configuration file
------------------------------------------------------------
 
--- Plugin: vim-signify 
+-- Plugin: vim-signify
 -- https://github.com/mhinz/vim-signify
 
-local g = vim.g
-local cmd = vim.cmd
+vim.g.signify_sign_add = "┃"
+vim.g.signify_sign_delete = "┃"
+vim.g.signify_sign_delete_first_line = "┃"
+vim.g.signify_sign_change = "┃"
+vim.g.signify_sign_change_delete = "┃"
 
-g.signify_sign_add = '┃'
-g.signify_sign_delete = '┃'
-g.signify_sign_delete_first_line = '┃'
-g.signify_sign_change = '┃'
-g.signify_sign_change_delete = '┃'
+-- vim.g.signify_sign_show_count = 1
 
--- g.signify_sign_show_count = 1
-
-cmd [[
+vim.cmd([[
   highlight SignifySignAdd ctermfg=green guifg=#76946A
   highlight SignifySignChange ctermfg=yellow guifg=#DCA561
   highlight SignifySignDelete ctermfg=red guifg=#C34043
   highlight link SignifySignChangeDelete SignifySignChange
   highlight link SignifySignDeleteFirstLine SignifySignDelete
-]]
+]])
