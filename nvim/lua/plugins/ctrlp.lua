302a92fafd3361c3d2abead8f1b9e68150d09550
Samantha Bartlett <sammynilla@protonmail.com>
2022-05-02 20:35:58 -0400

    minor ctrlp changes for now.

--- nvim/lua/plugins/ctrlp.lua
+++ nvim/lua/plugins/ctrlp.lua
@@ -2,22 +2,16 @@
 -- Fuzzy file finder configuration file
 -----------------------------------------------------------
 
--- Plugin: ctrlp.vim 
+-- Plugin: ctrlp.vim
 -- https://github.com/ctrlpvim/ctrlp.vim
 
-local g = vim.g
-local fn = vim.fn
+vim.g.ctrlp_line_prefix = "█ "
 
-g.ctrlp_line_prefix = '█ '
-
--- NOTE (sammynilla): MRU has to be available otherwise dev-icons fail to load.
--- g.ctrlp_types = {'fil', 'buf'}
-
-local rg_ignored_glob = '**/.git/*,**/node_modules/**'
-if fn.executable('rg') then
-  g.ctrlp_user_command = 'rg -. --files -g "!{'..rg_ignored_glob..'}"'
+local rg_ignored_glob = "**/.git/*,**/node_modules/**"
+if vim.fn.executable("rg") then
+  vim.g.ctrlp_user_command = "rg -. --files -g \"!{"..rg_ignored_glob.."}\""
 else
-  g.ctrlp_user_command = {
-    '.git', 'cd %s && git ls-files -co --exclude-standard'
+  vim.g.ctrlp_user_command = {
+    ".git", "cd %s && git ls-files -co --exclude-standard"
   }
 end
