cb827c2b428571368b5def908163c7cdfc226b8f
Samantha Bartlett <sammynilla@protonmail.com>
2022-05-11 04:24:25 -0400

    removed lsp loading from init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,13 +6,12 @@
 -- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 -- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
 
--- Latest Update: 2022/05/10
+-- Latest Update: 2022/05/11
 -- Maintainer: sammynilla
 -- Website: https://github.com/sammynilla/win-dotfiles/tree/main/nvim
 
 require("settings")
 require("plugins")
-require("lsp")
 require("keymaps")
 require("autocommands")
 
