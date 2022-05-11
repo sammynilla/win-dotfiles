7bf6105bf3a4c605f3b1d344508d03c724b699d9
Samantha Bartlett <sammynilla@protonmail.com>
2022-05-11 04:17:25 -0400

    .eslintrc and .prettierrc were already being seen as json files

--- nvim/ftdetect/json.lua
+++ nvim/ftdetect/json.lua
@@ -1,6 +1,6 @@
 
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
   command = "set filetype=json",
-  pattern = { ".eslintrc", ".prettierrc", "*.json*", },
+  pattern = { "*.json*", },
 })
 
