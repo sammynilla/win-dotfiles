7f247bfe6d279b85091dea69e411b4918951358c
Samantha Bartlett <sammynilla@protonmail.com>
2022-05-22 16:56:15 -0400

    added a small code snippet as an example of modifying lsp diagnostics

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -27,6 +27,15 @@ for _, lsp in ipairs(server_list) do
   require("lsp.servers." .. lsp).setup(on_attach, capabilities)
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    -- virtual_text = false,
+    -- signs = true,
+    -- update_in_insert = true,
+  }
+)
+
 local utils = require("utils")
 for type, icon in pairs(utils.signs) do
   local hl = "DiagnosticSign" .. type
